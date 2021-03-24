6b653b0dfc6b79581a0e28250bae774c82b2e0e0
Ralf Luther <luther.ralf@gmail.com>
2021-03-24 23:33:12 +0300

    instantnoodlep: decommonize PartsBin ramdisk
    Change-Id: Ibfbb00712cbd4340fd7b3987ad3a7f4f3d0ef3ec

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,6 +10,14 @@ LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.aicp-sm8250.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.aicp-sm8250.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE           := fstab.qcom.ramdisk
 LOCAL_MODULE_STEM      := fstab.qcom
 LOCAL_MODULE_TAGS      := optional
